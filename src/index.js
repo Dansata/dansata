541ac6ed56fa3724773b0aea74c772d72fb1c779
Daniel <talk@dansata.com>
2023-05-31 02:44:19 -0600

    Change work to portfolio and improve h1 size, ux copy enhanced

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom";
+import AnimatedCursor from "react-animated-cursor";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './MPI.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  Noise,
+  Cursor,
+  Navigation,
+  Footer,
+  Home,
+  About,
+  Portfolio,
+  Contact,
+  NotFoundPage,
+} from "./components";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Router>
+    <Noise />
+      <div className="Container">
+        <Navigation />
+        <AnimatedCursor
+      innerSize={8}
+      outerSize={32}
+      color='216, 123, 142'
+      outerAlpha={0.2}
+      innerScale={0.2}
+      outerScale={2}
+      clickables={[
+        'a',
+        'select',
+        'textarea',
+        'button',
+        '.link',
+        'span'
+      ]}
+    />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+        <Footer className="Footer" />
+      </div>
+  </Router>,
+
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
